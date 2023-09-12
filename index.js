17940d9eb16fe48019a8143dd8bf4ab5c0fed3c3
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-09-11 17:38:32 -0700

    Passed all tests

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 10
+let num4 = 14
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 10) + 1
+let mod = num4 % num3
+let max = Math.max(10,20)
